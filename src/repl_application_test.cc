a37d402cca1657b024d4a45cbab7100336ec88d2
Carl Johnson <cjvoid@gmail.com>
2012-12-21 16:14:27 +0000

    Add travis.yml file.

--- src/repl_application_test.cc
+++ src/repl_application_test.cc
@@ -5,11 +5,6 @@ namespace ajimu {
 namespace app {
 
 TEST(REPLTest, REPL) {
-	ReplApplication app;
-	ASSERT_TRUE(app.Init());
-	app.Load("src/lib/stdlib.scm");
-	fprintf(stdin, "(for-each (lambda (i) (display (list (ajimu.gc.state) (ajimu.gc.allocated)))) '(1 2 2 3 4 5 6 7 8 9 0))");
-	app.Run();
 }
 
 } // namespace app
